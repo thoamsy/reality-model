b3bd8bd94b1987c643c0c24c7085b7d3ac80b74b
thoamsy <c6swift@gmail.com>
2021-11-03 21:27:37 +0800

    Initial Commit

---
+++ RealModel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealModel
+//
+//  Created by yk on 2021/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealModel/RealModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealModelApp.swift
+//  RealModel
+//
+//  Created by yk on 2021/11/3.
+//
+
+import SwiftUI
+
+@main
+struct RealModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
